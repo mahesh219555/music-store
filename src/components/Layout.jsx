1e1f06706fc5ae8d9c7405db3ffbfff9f5c2d26d
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-06-03 16:09:45 -0400

    created header and layout components

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+
+const Layout = (props) => {
+  const { children } = props;
+
+  return (
+    <div>
+      <Header />
+      <div>
+        {children}
+      </div>
+    </div>
+  )
+
+};
+
+export default Layout;
